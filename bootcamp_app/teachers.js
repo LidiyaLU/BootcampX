5ada0ec77215ca50cd80a189c83053b28e1cf98a
LidiyaLU <cheerrlidiya@yandex.ru>
2020-11-29 03:39:52 +0000

    make parameterized SQL queries in app.js file

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,18 +8,27 @@ const pool = new Pool({
 });
 
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
-ORDER BY teacher;
-`)
-.then(res => { 
-  res.rows.forEach(teach => {
-    console.log(`${teach.teacher} : ${teach.cohort}`);
-  })
-  //console.log(res);
-});
+const cohortName = process.argv[2];
+
+const text = 'SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort FROM teachers JOIN assistance_requests ON teacher_id = teachers.id JOIN students ON student_id = students.id JOIN cohorts ON cohort_id = cohorts.id WHERE cohorts.name LIKE $1 ORDER BY teachers.name';
+
+const values = [`%${cohortName}%`];
+
+
+pool.query(text, values, (err, res) => {
+  if (err) {
+    console.log(err.stack);
+  } else {
+    //console.log(res.rows[0]);
+
+    res.rows.forEach(teach => {
+           console.log(`${teach.teacher} : ${teach.cohort}`);
+         })
+  }
+})
+// .then(res => { 
+//   res.rows.forEach(teach => {
+//     console.log(`${teach.teacher} : ${teach.cohort}`);
+//   })
+//   //console.log(res);
+// });
